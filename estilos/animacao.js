1eece996a12a92b2e8417f5dd8a38bce89924916
Arthur Bueno Vilas Boas <arthurbvb@hotmail.com>
2022-10-25 16:13:49 -0300

    att hora

--- estilos/animacao.js
+++ estilos/animacao.js
@@ -3,26 +3,27 @@ function carregar(){
     var msg = window.document.getElementById('hora')
     var foto = window.document.getElementById('foto')
     var data = new Date()
-    var hora = data.getHours().toFixed(2)
+    var hora = data.getHours()
+    var min = data.getMinutes()
     if(hora < 6){
         //madrugada
-        msg.innerHTML = `Agora são ${hora} horas. Boa madrugada!!!`
+        msg.innerHTML = `Agora são ${hora} : ${min}. Boa madrugada!!!`
         
     }else if(hora < 12 ){
         //bom dia
         foto.style.backgroundImage = "url(./Img/fundo-interativo/cafe.jpg)"
-        msg.innerHTML = `Agora são ${hora} horas. Bom dia!!!`
+        msg.innerHTML = `Agora são ${hora} : ${min}. Bom dia!!!`
     }else if(hora < 13){
         //almoço
         foto.style.backgroundImage = 'url(./Img/fundo-interativo/almoco.jpg)'
-        msg.innerHTML = `Agora são ${hora} horas. Bom almoço!!!`
+        msg.innerHTML = `Agora são ${hora} : ${min}. Bom almoço!!!`
     }else if(hora < 18){
         //tarde
         foto.style.backgroundImage = "url(./Img/fundo-interativo/tarde.jpg)"
-        msg.innerHTML = `Agora são ${hora} horas. Boa tarde!!!`
+        msg.innerHTML = `Agora são ${hora} : ${min}. Boa tarde!!!`
     } else{
         //noite
         foto.style.backgroundImage = 'url(./Img/fundo-interativo/noite.jpg)'
-        msg.innerHTML = `Agora são ${hora} horas. Boa noite!!!`
+        msg.innerHTML = `Agora são ${hora} : ${min}. Boa noite!!!`
     } 
 }
